3f543ab05998c7e1af1abf472f545a27feff8dc5
Chris Spanos <spanosc1@tcnj.edu>
2017-02-03 03:59:56 -0500

    working on mobile responsiveness

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,19 +1,74 @@
 $(document).ready(function() {
-	var imagePositionMain = 1;
+  var mq = window.matchMedia( "(min-width: 992px)" );
+	if(mq.matches)
+  {
+    //CAROUSEL
+    //add more images here
+    var imageURLSMain = [
+      'joshua.png',
+      '424075_4788125592391_328148535_n.jpg',
+      '374547_4510475211305_454145329.jpg'
+    ];
+
+    setInterval(function() {
+      if(imagePositionMain == imageURLSMain.length)
+      {
+        imagePositionMain = 0;
+      }
+      $("#profileImage").animate({
+        opacity: "0"
+      }, 1000, function() {
+        $("#profileImage").attr("src", "images/" + imageURLSMain[imagePositionMain]);
+        imagePositionMain++;
+      });
+      $("#profileImage").animate({
+        opacity: "1.0"
+      }, 1000);
+    }, 7000);
+    //END OF CAROUSEL
+    //Place starting image and description
+    
+    //CAROUSEL
+    //add more images here
+    var imageURLSBio = [
+      'img_0268_2.jpg',
+      '421231_3232546823894_155990807.jpg',
+      '68298_302516633182727_12237966.jpg',
+      '549642_10201037261398478_68705.jpg'
+    ];
+
+    setInterval(function() {
+      if(imagePositionBio == imageURLSBio.length)
+      {
+        imagePositionBio = 0;
+      }
+      $("#bioImage").animate({
+        opacity: "0"
+      }, 1000, function() {
+        $("#bioImage").css("background-image", "url('images/" + imageURLSBio[imagePositionBio] + "')");
+        imagePositionBio++;
+      });
+      $("#bioImage").animate({
+        opacity: "1.0"
+      }, 1000);
+    }, 7000);
+    //END OF CAROUSEL
+  }
+  var imagePositionMain = 1;
   var imagePositionBio = 1;
   $("#jumbo").animate({
     marginTop: "+=10px",
     opacity: "1.0"
   }, 1000, function() {
-  	$("#outerImageDiv").animate({
-  		marginLeft: "0px",
-  		opacity: "1.0"
-  	}, 1200 );
-  	$("#outerHomeText").animate({
-  		marginRight: "0px",
-  		marginLeft: "0px",
-  		opacity: "1.0"
-  	}, 1200, function() {
+    $("#outerImageDiv").animate({
+      marginLeft: "0px",
+      opacity: "1.0"
+    }, 1200 );
+    $("#outerHomeText").animate({
+      marginRight: "0px",
+      marginLeft: "0px",
+      opacity: "1.0"
+    }, 1200, function() {
       $("#bioDesc").animate({
         top: "+=70px",
         opacity: "1.0"
@@ -23,55 +78,4 @@ $(document).ready(function() {
       }, 1000);
     });
   });
-  //CAROUSEL
-  //add more images here
-  var imageURLSMain = [
-    'joshua.png',
-    '424075_4788125592391_328148535_n.jpg',
-    '374547_4510475211305_454145329.jpg'
-  ];
-
-  setInterval(function() {
-    if(imagePositionMain == imageURLSMain.length)
-    {
-      imagePositionMain = 0;
-    }
-    $("#profileImage").animate({
-      opacity: "0"
-    }, 1000, function() {
-      $("#profileImage").attr("src", "images/" + imageURLSMain[imagePositionMain]);
-      imagePositionMain++;
-    });
-    $("#profileImage").animate({
-      opacity: "1.0"
-    }, 1000);
-  }, 7000);
-  //END OF CAROUSEL
-  //Place starting image and description
-  
-  //CAROUSEL
-  //add more images here
-  var imageURLSBio = [
-    'img_0268_2.jpg',
-    '421231_3232546823894_155990807.jpg',
-    '68298_302516633182727_12237966.jpg',
-    '549642_10201037261398478_68705.jpg'
-  ];
-
-  setInterval(function() {
-    if(imagePositionBio == imageURLSBio.length)
-    {
-      imagePositionBio = 0;
-    }
-    $("#bioImage").animate({
-      opacity: "0"
-    }, 1000, function() {
-      $("#bioImage").css("background-image", "url('images/" + imageURLSBio[imagePositionBio] + "')");
-      imagePositionBio++;
-    });
-    $("#bioImage").animate({
-      opacity: "1.0"
-    }, 1000);
-  }, 7000);
-  //END OF CAROUSEL
 });
